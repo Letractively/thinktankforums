a0cda4b60f90579749372816b34bf98a0b2feac9
dbrown29 <dbrown29@510118ab-9126-0410-8522-355556fb886b>
2008-11-16 21:52:55 +0000

    added heredocs to improve code readability. see: http://us3.php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc

--- branches/djb-01-heredoc/include_footer.php
+++ branches/djb-01-heredoc/include_footer.php
@@ -11,7 +11,9 @@
 <?php
 $time_end = microtime(true);
 $time = $time_end - $time_start;
-echo "<!-- page generated in $time seconds\n";
-echo "     by think tank forums {$ttf_cfg["version"]}\n";
-echo "     visit http://www.ttfproject.com/ -->\n";
+print <<<EOF
+<!-- page generated in $time seconds
+     by think tank forums {$ttf_cfg["version"]}
+     visit http://www.ttfproject.com/ -->
+EOF
 ?>
