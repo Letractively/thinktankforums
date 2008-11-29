1ec3073c5372ec5e5397db24668a57b59a235b37
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-11-29 16:34:54 +0000

    switch to php and push some utf-8 action.

--- ttfproject/index.php
+++ ttfproject/index.php
@@ -1,7 +1,8 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>The Think Tank Forums Project: Web forum software for the data obsessed</title>
         <link rel="stylesheet" type="text/css" href="style.css" />
         <link rel="icon" type="image/png" href="/images/icon.png" />
