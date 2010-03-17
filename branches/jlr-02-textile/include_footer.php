13ae2b24ff197cce21396ff9b99893b0ca483c44
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2010-03-17 03:19:26 +0000

    let's work on textile.

---
+++ branches/jlr-02-textile/include_footer.php
@@ -0,0 +1,25 @@
+<?php
+/* think tank forums
+ *
+ * include_footer.php
+ */
+
+echo <<<EOF
+            <br style="clear: both;" />
+        </div>
+    </body>
+</html>
+
+EOF;
+
+$time_end = microtime(true);
+$time = $time_end - $time_start;
+
+echo <<<EOF
+<!-- page generated in $time seconds
+     by think tank forums {$ttf_cfg["version"]}
+     visit http://www.ttfproject.com/ -->
+
+EOF;
+
+?>
